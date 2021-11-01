e60f0a51b48fef60557d78b2df179bed98a79b69
Eugene Kubesh <codbasters@gmail.com>
2021-11-01 12:28:41 +0300

    add class exports for exported components

--- src/public_api.ts
+++ src/public_api.ts
@@ -3,3 +3,7 @@
  */
 
 export { FsColorPickerModule } from './app/fs-color-picker.module';
+
+export { DialogComponent } from './app/components/dialog/dialog.component';
+export { FsColorPickerComponent } from './app/components/color-picker/color-picker.component';
+export { FsColorPickerChipComponent } from './app/components/color-picker-chip/color-picker-chip.component';
